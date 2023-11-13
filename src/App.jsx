71dfc65155663e41c0aa0e079f4e85dbf1036c4d
Melissa <150301466+Uniiris@users.noreply.github.com>
2023-11-13 23:21:02 +0000

    Desafio 1 CSS corrigido

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ function App() {
   return (
     <>
       <div className="card">
-        <h2>Rick Sanchez</h2>
+        <div id='linha-nome'>
+          <h2>Rick Sanchez</h2>
+          </div>
+          <div className='status'></div>
+            <div className='tag'>Status: Vivo</div>
+            <div className='tag'>Humana</div> <p></p>
+            <div className='tag'>Terra C137</div> <p></p>
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
       </div>
     </>
