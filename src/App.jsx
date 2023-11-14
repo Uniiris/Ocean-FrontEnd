439000eb9bc2d85360773b0094ca7416f6b2aadd
Melissa <150301466+Uniiris@users.noreply.github.com>
2023-11-14 00:46:33 +0000

    Extração do conteúdo para o '<Card />'

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,8 @@ import Card from './components/Card/Card'
 function App() {
   return (
     <>
-      <div className="card">
-        <div id='linha-nome'>
-          <h2>Rick Sanchez</h2>
-          </div>
-          <div className='tags'>
-            <div className='tag'>Status: Vivo</div>
-            <div className='tag'>Humana</div>
-            <div className='tag'>Terra C137</div>
-          </div>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
-      </div>
+      
+      <Card />
       <Card />
     </>
   )
