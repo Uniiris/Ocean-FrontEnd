75af476eb73b75fc603e5ed873eaaacff4cd8792
Melissa <150301466+Uniiris@users.noreply.github.com>
2023-11-14 00:02:21 +0000

    Criação do componente <Card>

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
   return (
@@ -7,12 +8,14 @@ function App() {
         <div id='linha-nome'>
           <h2>Rick Sanchez</h2>
           </div>
-          <div className='status'></div>
+          <div className='tags'>
             <div className='tag'>Status: Vivo</div>
-            <div className='tag'>Humana</div> <p></p>
-            <div className='tag'>Terra C137</div> <p></p>
+            <div className='tag'>Humana</div>
+            <div className='tag'>Terra C137</div>
+          </div>
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
       </div>
+      <Card />
     </>
   )
 }
