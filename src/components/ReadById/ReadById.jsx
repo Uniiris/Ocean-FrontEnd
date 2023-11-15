95b727bbdc93448d49db4bd2ab4c824b97b4d234
Melissa <150301466+Uniiris@users.noreply.github.com>
2023-11-15 01:30:28 +0000

    Continuação ReadById

--- src/components/ReadById/ReadById.jsx
+++ src/components/ReadById/ReadById.jsx
@@ -3,5 +3,10 @@ import { useParams } from "react-router-dom"
 export default function ReadById() {
     const { id } = useParams()
 
+    const apiUrl = `https://rickandmortyapi.com/api/character/${id}`
+    // TODO: Implementar o carregamento da API
+    // TODO: Guardar a informação recebida da API em um estado
+    // TODO: Exibir essas informações no JSX
+
     return <div>Hello { id }</div>
 }
