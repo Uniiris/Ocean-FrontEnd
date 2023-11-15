6da65da6f27308489780581470044ff75532448e
Melissa <150301466+Uniiris@users.noreply.github.com>
2023-11-15 01:01:43 +0000

    Erro

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home/Home';
 
 export default function App() {
   return (
-    <>
-      App
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
