e741cb5844f2e6ea2f9c16b414e3dffaea5f4d3a
Melissa <150301466+Uniiris@users.noreply.github.com>
2023-11-15 00:45:06 +0000

    Criação do arquivo Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,9 @@
-import { useEffect } from 'react'
 import './App.css'
-import Card from './components/Card/Card'
 
-function App() {
-  const [items, setItems] = useState([])
-
-  async function carregarDadosApi() { 
-    const apiUrl = "https://rickandmortyapi.com/api/character/"
-
-    const response = await fetch(apiUrl)
-
-    const body = await response.json()
-
-    const results = body.results.map(function (element) {
-      return {
-        name: element.name,
-        image: element.image,
-        tags: [
-          `Status: ${element.status}`,
-          `Species: ${element.species}`,
-          `Origin: ${element.origin.name}`,
-          `Episodes: ${element.episode.length}`
-        ]
-      }
-    })
-
-    setItems(results)
-  }
-
-  useEffect(function () {
-    carregarDadosApi()
-  }, [])
-
-  carregarDadosApi()
-
+export default function App() {
   return (
     <>
-    <div className='cards'>
-      {items.map(function(element) {
-        return <Card item={element} />
-      })}
-      </div>
+      App
     </>
   )
 }
-
-export default App;
