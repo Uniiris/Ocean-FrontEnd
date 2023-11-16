ea432f25fbfdd64f02120ba0423b6096fee49893
Melissa <150301466+Uniiris@users.noreply.github.com>
2023-11-16 23:18:17 +0000

    Erro

--- src/components/ReadById/ReadById.jsx
+++ src/components/ReadById/ReadById.jsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 export default function ReadById() {
     const { id } = useParams()
 
+    const[item, setItem] = useState({})
+
     const apiUrl = `https://rickandmortyapi.com/api/character/${id}`
+
+    const response = await fetch(apiUrl)
+
+    const body = await response.json()
+
+    setItem(body)
+
+    console.log(body)
+
+    useEffect(function() {
+        carregarDadosApi()
+    }, [])
     // TODO: Implementar o carregamento da API
     // TODO: Guardar a informação recebida da API em um estado
     // TODO: Exibir essas informações no JSX
 
-    return <div>Hello { id }</div>
+    return <div> Hello { id } - {item.name}</div>
 }
