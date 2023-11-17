cf235f9ff598759dfd12dea5be92dfc60c7cea11
Melissa <150301466+Uniiris@users.noreply.github.com>
2023-11-17 00:46:49 +0000

    Estilização ReadById

--- src/components/ReadById/ReadById.jsx
+++ src/components/ReadById/ReadById.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import "./ReadById.css"
 
 export default function ReadById() {
-    const { id } = useParams()
+  const { id } = useParams()
 
-    const[item, setItem] = useState({})
+  const [item, setItem] = useState({})
 
-    async function carregarDadosApi() {
-
+  async function carregarDadosApi() {
     const apiUrl = `https://rickandmortyapi.com/api/character/${id}`
 
     const response = await fetch(apiUrl)
@@ -15,16 +15,47 @@ export default function ReadById() {
     const body = await response.json()
 
     setItem(body)
+  }
 
-    console.log(body)
-    }
+  useEffect(function () {
+    carregarDadosApi()
+  }, [])
 
-    useEffect(function() {
-        carregarDadosApi()
-    }, [])
-    // TODO: Implementar o carregamento da API
-    // TODO: Guardar a informação recebida da API em um estado
-    // TODO: Exibir essas informações no JSX
+  // TODO: Exibir essas informações no JSX
 
-    return <div> Hello { id } - {item.name}</div>
+  // return <div>ReadById: {id} - {item.name}</div>
+
+  return (
+    <div className="main">
+      <div className="image-wrapper">
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Imagem do Personagem" />
+      </div>
+
+      <div className="info">
+        <div className="info-tag">Status: Alive</div>
+
+        <div className="info-title">Rick Sanchez</div>
+
+        <div className="info-row">
+          <div className="info-label">Species:</div>
+          <div className="info-text">Human</div>
+        </div>
+
+        <div className="info-row">
+          <div className="info-label">Gender:</div>
+          <div className="info-text">Male</div>
+        </div>
+
+        <div className="info-row">
+          <div className="info-label">Origin:</div>
+          <div className="info-text">Earth (C-137)</div>
+        </div>
+
+        <div className="info-row">
+          <div className="info-label">Location:</div>
+          <div className="info-text">Citadel of Ricks</div>
+        </div>
+      </div>
+    </div>
+  )
 }
